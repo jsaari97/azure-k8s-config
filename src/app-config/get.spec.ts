67e0269a76af08b2362b4d679f026eec28707a03
Jim Saari <jim.saari97@gmail.com>
2020-06-09 18:03:59 +0300

    Add get util tests

---
+++ src/app-config/get.spec.ts
@@ -0,0 +1,9 @@
+import { getValue } from "./get";
+
+describe("Azure: Get Value", () => {
+  it("should return app config value", async () => {
+    const value = await getValue("my-key");
+
+    expect(value).toEqual("my-value");
+  });
+});
