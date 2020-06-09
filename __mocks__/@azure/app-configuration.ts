7f21301a3c7382368b69eee37106b61f719d0f85
Jim Saari <jim.saari97@gmail.com>
2020-06-09 18:03:25 +0300

    Add azure mocks

---
+++ __mocks__/@azure/app-configuration.ts
@@ -0,0 +1,7 @@
+export class AppConfigurationClient {
+  getConfigurationSetting() {
+    return {
+      value: "my-value",
+    };
+  }
+}
---
+++ __mocks__/@azure/keyvault-secrets.ts
@@ -0,0 +1,5 @@
+export class SecretClient {
+  getSecret() {
+    return "my-secret";
+  }
+}
