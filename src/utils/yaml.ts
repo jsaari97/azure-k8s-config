a48b52f750ba84146ab32bbee398d110fae1ddbc
Jim Saari <jim.saari97@gmail.com>
2021-01-03 22:11:05 +0200

    Upgrade to js-yaml@^4

--- src/utils/yaml.ts
+++ src/utils/yaml.ts
@@ -1,7 +1,7 @@
-import Yaml from "js-yaml";
+import yaml from "js-yaml";
 import { SecretConfig } from "../types";
 
 export const parseYaml = (data: string): SecretConfig =>
-  Yaml.safeLoad(data) as SecretConfig;
+  yaml.load(data) as SecretConfig;
 
-export const formatYaml = (data: SecretConfig): string => Yaml.safeDump(data);
+export const formatYaml = (data: SecretConfig): string => yaml.dump(data);
